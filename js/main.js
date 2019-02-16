948271b331f49b63595f276802b543fa8a63b789
Dinosaur DBA <dinosaurdba@gmail.com>
2019-02-16 20:45:52 +0000

    Add Sohm Mohk Soo array

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ const msg = new SpeechSynthesisUtterance();
 msg.volume = 1;
 msg.rate = 1;
 msg.pitch = 1;
-//msg.text = "Key Bon Soo";
 msg.voiceURI = "Fiona";
 msg.lang = "en-scotland";
 
-// Init the Ki Bon Soo array
+// Init the Ki Bohn Soo array
 const kibonsoo =   ['key bon soo one',
                     'key bon soo two',
                     'key bon soo three',
@@ -25,6 +24,19 @@ const kibonsoo =   ['key bon soo one',
                     'key bon soo thirteen',
                     'key bon soo fourteen',
                     'key bon soo fifteen'];
+
+// Init the Sohn Mohk Soo array
+const sohnmohksoo =    ['sohn mock soo one',
+                        'sohn mock soo two',
+                        'sohn mock soo three',
+                        'sohn mock soo four',
+                        'sohn mock soo five',
+                        'sohn mock soo six',
+                        'sohn mock soo seven',
+                        'sohn mock soo eight',
+                        'sohn mock soo nine',
+                        'sohn mock soo ten',
+                        'sohn mock soo eleven'];
 
 function shuffle(arra1) {
     let ctr = arra1.length;
@@ -75,119 +87,11 @@ button.addEventListener('click', function(e) {
 for (var i=0; i<5; i++) {
     speechSynthesis.speak(new SpeechSynthesisUtterance(shuffledkbs[i]))
 }
-*/
 
 
-
-/*
 // Init SpeachSynth APE
 const synth = window.speechSynthesis;
 
-// DOM elements
-const textForm = document.querySelector('form');
-const voiceSelect = document.querySelector('#voice-select');
-
-// Init the Ki Bon Soo array
-const kibonsoo =   ['key bon soo one',
-                    'key bon soo two',
-                    'key bon soo three',
-                    'key bon soo four',
-                    'key bon soo five',
-                    'key bon soo six',
-                    'key bon soo seven',
-                    'key bon soo eight',
-                    'key bon soo nine',
-                    'key bon soo ten',
-                    'key bon soo eleven',
-                    'key bon soo twelve',
-                    'key bon soo thirteen',
-                    'key bon soo fourteen',
-                    'key bon soo fifteen'];
-
 var randkbs = kibonsoo[Math.floor(Math.random() * kibonsoo.length)];
 
-// Init the voices array
-let voices = [];
-
-const getVoices =  () => {
-    voices = synth.getVoices();
-    
-    // Loop through voices and create an option for each one
-    voices.forEach(voice => {
-        // Create option element
-        const option = document.createElement('option');
-        // Fill option with the voice and language
-        option.textContent = voice.name + '('+ voice.lang +')';
-
-        // Set needed option attributes
-        option.setAttribute('data-lang', voice.lang);
-        option.setAttribute('data-name', voice.name);
-        voiceSelect.appendChild(option);
-    });
-};
-
-getVoices();
-if (synth.onvoiceschanged !== undefined) {
-    synth.onvoiceschanged = getVoices;
-}
-
-
-// Speak
-const speak = () => {
-    // Check if speaking
-    if(synth.speaking) {
-        console.error('Already speaking...');
-        return;
-    }
-    //if(textInput.value !== '') {
-        // Get speak text
-        // const speakText = new SpeechSynthesisUtterance(textInput.value);
-        const speakText = new SpeechSynthesisUtterance(randkbs);
-        // Speak end
-        speakText.onend = e => {
-            console.log('Done speaking...');
-    //    }
-
-        // Speak error
-        speakText.onerror = e => {
-            console.error('Something went wrong');
-        }
-
-        // Selected voice
-        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
-
-        // Loop through voices
-        voices.forEach(voice => {
-            if(voice.name === selectedVoice) {
-                speakText.voice = voice;
-            }
-        });
-
-        // Set pitch and rate
-        //speakText.rate = rate.value;
-        speakText.rate = 1;
-        //speakText.pitch = pitch.value;
-        speakText.pitch = 1;
-        // Speak
-        synth.speak(speakText);
-    }
-};
-
-// EVENT LISTENER
-
-// Text form submit
-//textForm.addEventListener('submit', e => {
-//    e.preventDefault();
-//    speak();
-//    textInput.blur();
-//});
-
-// Rate value change
-//rate.addEventListener('change', e => rateValue.textContent = rate.value)
-
-// Pitch value change
-//pitch.addEventListener('change', e=> pitchValue.textContent = pitch.value)
-
-// Voice select change
-voiceSelect.addEventListener('change', e => speak());
 */
